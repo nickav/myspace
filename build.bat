99ef61a2078d8f7eba919600b75348ffcd607eec
Nick Aversano <nick23hi@gmail.com>
2021-07-22 14:38:00 -0400

    Initial commit

---
+++ build.bat
@@ -0,0 +1,26 @@
+@echo off
+
+:: Setup
+set script_path=%~dp0%
+set project_root=%script_path%
+
+:: Config
+set build_folder=%project_root%\build\
+set bin_folder=%project_root%\bin\
+set src_file=%project_root%\src\main.cpp
+set exe_name=cyan_debug.exe
+
+:: Build executable
+if not exist %build_folder% (mkdir %build_folder%)
+
+pushd %build_folder%
+  cl /MD -DDEBUG=1 /Od -nologo %src_file% /link -subsystem:console -incremental:no -opt:ref -OUT:%exe_name%
+
+  IF %errorlevel% NEQ 0 (popd && goto end)
+
+  .\%exe_name%
+popd
+
+:end
+
+exit /B %errorlevel%
---
+++ src/main.cpp
@@ -0,0 +1,355 @@
+#include <stdio.h>
+
+//
+// Basic
+//
+
+#ifndef NULL
+  #define NULL 0
+#endif
+
+#ifndef null
+  #define null nullptr
+#endif
+
+#define print printf
+
+#ifdef DEBUG
+  #define assert(expr) do { if (!(expr)) { print("%s %d: assert failed: %s\r\n", __FILE__, __LINE__, #expr); fflush(stdout); *(volatile int *)0 = 0; } } while (0)
+#else
+  #define assert(expr)
+#endif
+
+#define cast(type) (type)
+
+#define count_of(array) (sizeof(array) / sizeof((array)[0]))
+
+#define kilobytes(value) (value * 1024LL)
+#define megabytes(value) (value * 1024LL * 1024LL)
+#define gigabytes(value) (value * 1024LL * 1024LL * 1024LL)
+#define terabytes(value) (value * 1024LL * 1024LL * 1024LL * 1024LL)
+
+
+#if defined(_WIN32)
+  #define OS_WIN32 1
+#elif defined(__APPLE__)
+  #define OS_MACOS 1
+#elif defined(__linux__)
+  #define OS_LINUX 1
+#else
+  #error "[OS] Unsupported operating system!"
+#endif
+
+#ifndef OS_WIN32
+  #define OS_WIN32 0
+#endif
+
+#ifndef OS_MACOS
+  #define OS_MACOS 0
+#endif
+
+#ifndef OS_LINUX
+  #define OS_LINUX 0
+#endif
+
+//
+// Types
+//
+#include <stdint.h>
+
+typedef uint8_t  u8;
+typedef int8_t   i8;
+typedef uint16_t u16;
+typedef int16_t  i16;
+typedef uint32_t u32;
+typedef int32_t  i32;
+typedef uint64_t u64;
+typedef int64_t  i64;
+
+typedef float    f32;
+typedef double   f64;
+
+#define U8_MAX  0xff
+#define I8_MAX  0x7f
+#define I8_MIN  0x80
+#define U16_MAX 0xffff
+#define I16_MAX 0x7fff
+#define I16_MIN 0x8000
+#define U32_MAX 0xffffffff
+#define I32_MAX 0x7fffffff
+#define I32_MIN 0x80000000
+#define U64_MAX 0xffffffffffffffff
+#define I64_MAX 0x7fffffffffffffff
+#define I64_MIN 0x8000000000000000
+
+#define F32_MIN FLT_MIN
+#define F32_MAX FLT_MAX
+#define F64_MIN DBL_MIN
+#define F64_MAX DBL_MAX
+
+//
+// Memory
+//
+void *memory_copy(void *from, void *to, u64 size) {
+  u8 *src = cast(u8 *)from;
+  u8 *dest = cast(u8 *)to;
+
+  while (size--) { *dest++ = *src++; }
+
+  return to;
+}
+
+bool memory_equals(void *a_in, void *b_in, u64 size) {
+  u8 *a = cast(u8 *)a_in;
+  u8 *b = cast(u8 *)b_in;
+
+  while (size--) {
+    if (*a++ != *b++) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+void memory_set(void *ptr, u8 value, u64 size) {
+  u8 *at = cast(u8 *)ptr;
+  while (size--) { *at++ = value; }
+}
+
+void memory_zero(void *ptr, u64 size) {
+  u8 *at = cast(u8 *)ptr;
+  while (size--) { *at++ = 0; }
+}
+
+//
+// Context
+//
+
+struct Arena {
+  u8 *data;
+  u64 offset;
+  u64 size;
+};
+
+const u64 temporary_storage_size = megabytes(32);
+u8 temporary_storage_buffer[temporary_storage_size];
+Arena global_temporary_storage = {temporary_storage_buffer, 0, temporary_storage_size};
+
+void *talloc(u64 size) {
+  Arena *arena = &global_temporary_storage;
+  void *result = NULL;
+
+  if (arena->offset + size < arena->size) {
+    result = arena->data + arena->offset;
+    arena->offset += size;
+  }
+
+  return result;
+}
+
+void reset_temporary_storage() {
+  Arena *arena = &global_temporary_storage;
+  arena->offset = 0;
+}
+
+//
+// String
+//
+struct String {
+  u64 count;
+  u8 *data;
+
+  u8 &operator[](u64 i) {
+    assert(i >= 0 && i < count);
+    return data[i];
+  }
+};
+
+#define S(x) String{sizeof(x) - 1, cast(u8 *)x}
+
+#define LIT(str) cast(int)str.count, cast(char *)str.data
+
+struct String16 {
+  u64 count;
+  u16 *data;
+};
+
+struct String32 {
+  u64 count;
+  u32 *data;
+};
+
+struct StringNode {
+  String it;
+  String *next;
+};
+
+struct StringList {
+  StringNode *first;
+  StringNode *last;
+  u32 count;
+  u32 size_in_bytes;
+};
+
+struct StringDecode {
+  u32 codepoint;
+  u8 size; // 1 - 4
+};
+
+char *cstr(String str) {
+  if (!str.count || !str.data) {
+    return NULL;
+  }
+
+  char *result = cast(char *)talloc(str.count + 1); // size for null character
+  memory_copy(str.data, result, str.count);
+  result[str.count] = 0;
+  return result;
+}
+
+StringDecode string_decode_utf8(u8 *str, u32 capacity) {
+  static u8 high_bits_to_count[] = {
+    1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    2, 2, 2, 2, 3, 3, 4, 0,
+  };
+
+  StringDecode result = {'?', 1};
+
+  u8 count = high_bits_to_count[str[0] >> 3];
+
+  // @Speed: this could be made branchless
+  if (capacity >= count) {
+    switch (count) {
+      case 1: {
+        result.size = 1;
+        result.codepoint = str[0] & 0x7f;
+      } break;
+
+      case 2: {
+        result.size = 2;
+        result.codepoint = ((str[0] & 0x1f) << 6) | (str[1] & 0x3f);
+      } break;
+
+      case 3: {
+        result.size = 3;
+        result.codepoint = ((str[0] & 0x0f) << 12) | ((str[1] & 0x3f) << 6) | (str[2] & 0x3f);
+      } break;
+
+      case 4: {
+        result.size = 4;
+        result.codepoint = ((str[0] & 0x09) << 18) | ((str[1] & 0x3f) << 12) | ((str[2] & 0x3f) << 6) | (str[3] & 0x3f);
+      } break;
+    }
+  }
+
+  return result;
+}
+
+u32 string_encode_utf8(u8 *dest, u32 codepoint) {
+  u32 size = 0;
+
+  if (codepoint <= 0x7f) {
+    size = 1;
+    dest[0] = codepoint;
+  }
+  else if (codepoint <= 0x7ff) {
+    size = 2;
+    dest[0] = 0xC0 | (codepoint >> 6);
+    dest[1] = 0x80 | (codepoint & 0x3f);
+  }
+  else if (codepoint <= 0xffff) {
+    size = 3;
+    dest[0] = 0xE0 | (codepoint >> 12);
+    dest[1] = 0x80 | ((codepoint >> 6) & 0x3f);
+    dest[2] = 0x80 | (codepoint & 0x3f);
+  }
+  else if (codepoint <= 0x10FFFF) {
+    size = 4;
+    dest[0] = 0xF0 | (codepoint >> 18);
+    dest[1] = 0x80 | ((codepoint >> 12) & 0x3f);
+    dest[2] = 0x80 | ((codepoint >> 6) & 0x3f);
+    dest[3] = 0x80 | (codepoint & 0x3f);
+  }
+  else {
+    dest[0] = '?';
+    size = 1;
+  }
+
+  return size;
+}
+
+//
+// OS
+//
+
+#if OS_WIN32
+
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <windows.h>
+
+String os_read_entire_file(String path) {
+  String result = {};
+
+  //HANDLE handle = CreateFileW();
+
+  return result;
+}
+
+#endif
+
+#if 0
+bool os_make_directory(String path) {
+}
+
+bool os_remove_directory(String path) {
+}
+#endif
+
+//
+// Functions
+//
+
+int main(int argc, char **argv) {
+  print("hello, sailor!\n");
+
+  {
+    u8 buf1[] = {127};
+    auto res1 = string_decode_utf8(buf1, count_of(buf1));
+    print("res1: %llu (%d)\n", res1.codepoint, res1.size);
+
+    u8 buf2[] = {0xC2, 0xA2};
+    auto res2 = string_decode_utf8(buf2, count_of(buf2));
+    print("res2: %llu (%d)\n", res2.codepoint, res2.size);
+
+    u8 buf3[] = {0xE2, 0x82, 0xAC};
+    auto res3 = string_decode_utf8(buf3, count_of(buf3));
+    print("res3: %llu (%d)\n", res3.codepoint, res3.size);
+
+    u8 buf4[] = {0xF0, 0x90, 0x8D, 0x88};
+    auto res4 = string_decode_utf8(buf4, count_of(buf4));
+    print("res4: %llu (%d)\n", res4.codepoint, res4.size);
+
+    u8 enc1[4] = {};
+    string_encode_utf8(enc1, res1.codepoint);
+    assert(memory_equals(buf1, enc1, count_of(buf1)));
+
+    u8 enc2[4] = {};
+    string_encode_utf8(enc2, res2.codepoint);
+    assert(memory_equals(buf2, enc2, count_of(buf2)));
+
+    u8 enc3[4] = {};
+    string_encode_utf8(enc3, res3.codepoint);
+    assert(memory_equals(buf3, enc3, count_of(buf3)));
+
+    u8 enc4[4] = {};
+    string_encode_utf8(enc4, res4.codepoint);
+    assert(memory_equals(buf4, enc4, count_of(buf4)));
+    print("enc4: %X %X %X %X\n", enc4[0], enc4[1], enc4[2], enc4[3]);
+  }
+
+  return 0;
+}
