434b3cf6b2df16ded17d372b4d9695167fef750e
Nick Aversano <nick23hi@gmail.com>
2023-02-24 20:00:14 -0500

    add publish script

---
+++ dist.bat
@@ -0,0 +1,27 @@
+@echo off
+
+:: Setup
+set project_root=%~dp0%
+set publish_path=%project_root%..\nickav.github.io
+
+echo From: %project_root%
+echo To: %publish_path%
+
+pushd %project_root%
+
+    FOR /F "tokens=*" %%g IN ('more %project_root%\.git\refs\heads\master') do (set build_hash=%%g)
+
+    echo %build_hash%
+
+    FOR /F "tokens=*" %%g IN ('more %project_root%\.git\COMMIT_EDITMSG') do (set commit_message=%%g)
+
+    echo %commit_message%
+popd
+
+pushd %publish_path%
+    xcopy /s /i /y %project_root%\build\bin %publish_path%
+
+    git commit -a -m "%commit_message%"
+
+     git push
+popd
